5984482f6e92061b68a116e25c148863750c6084
Biruk Bizuayehu <brookbizuayehu@gmail.com>
2024-06-08 11:09:14 +0300

    sql analysis

--- EmployeeSQL/employee_analysis.sql
+++ EmployeeSQL/employee_analysis.sql
@@ -1,6 +1,6 @@
 -- Data Analysis
 
--- List the employee number, last name, first name, sex, and salary of each employee.
+-- Question 1. List the employee number, last name, first name, sex, and salary of each employee.
 
 SELECT employee.emp_no
 	,employee.last_name
@@ -10,15 +10,15 @@ SELECT employee.emp_no
 FROM employees AS employee
 INNER JOIN salaries AS salary ON salary.emp_no = employee.emp_no;
 
--- List the first name, last name, and hire date for the employees who were hired in 1986.
+-- Question 2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT 
     first_name, 
     last_name,
     hire_date
 FROM employees 
-WHERE hire_date >= '1986-01-01' AND emp.hire_date <= '1986-12-31'
+WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31'
 
--- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+-- Question 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
 
 SELECT departments.dept_no,
        departments.dept_name,
@@ -26,9 +26,19 @@ SELECT departments.dept_no,
        employees.last_name,
        employees.first_name
 FROM departments departments
-    INNER JOIN dept_manager dept_manager
+    INNER JOIN department_manager dept_manager
         ON departments.dept_no = dept_manager.dept_no
     INNER JOIN employees employees
         ON dept_manager.emp_no = employees.emp_no;
 
-        
+-- Question 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+SELECT dept_emp.emp_no,
+       employees.last_name,
+       employees.first_name,
+       departments.dept_name
+FROM department_Employee dept_emp
+    INNER JOIN employee
+        ON dept_emp.emp_no = employee.emp_no
+    INNER JOIN departments
+        ON dept_emp.dept_no = departments.dept_no;
